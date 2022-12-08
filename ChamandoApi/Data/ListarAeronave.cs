611a54674555f07bcd84c75e9c0b5f3959fc2566
DiogoEverson <113920697+DiogoEvs@users.noreply.github.com>
2022-12-08 11:14:40 -0300

    Add files via upload

---
+++ ChamandoApi/Data/ListarAeronave.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+	public class ListarAeronave
+	{
+		[JsonProperty("Id")]
+		public int Id { get; set; }
+
+		[JsonProperty("Modelo")]
+		public string? Modelo { get; set; }
+
+		[JsonProperty("Codigo")]
+		public string? Codigo { get; set;}
+	}
+
---
+++ ChamandoApi/Data/Login.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+	public class Login
+	{
+		[JsonProperty("Usuario")]
+		public string? Usuario { get; set; }
+
+		[JsonProperty("Senha")]
+		public string? Senha { get; set; }
+
+		[JsonProperty("DataCriacao")]
+		public DateTime DataCriacao { get; set; }
+	}
